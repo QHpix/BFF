6b0ebb10b9f5023f6b3a272560649f54a3e54a97
L00P3R <anon.looper@gmail.com>
2018-05-22 21:39:52 +0200

    Added send/recv functions

--- v0.01/Handler.cpp
+++ v0.01/Handler.cpp
@@ -1,4 +1,17 @@
 #include "lib/Handler.h"
+
 Handler::Handler(){
 
+}
+/*
+* Alpha *
+Returns 0 is all went correct
+*/
+int Handler::recv(char *dest, uint64_t n){
+  read(_pipe.in, dest, n);
+}
+
+
+void Handler::send(const char *data, uint64_t n){
+  write(_pipe.out, data, n);
 }
